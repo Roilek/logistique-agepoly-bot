bc1fcb74ec46b8a41f8d60d1d06db35a14d5304e
Roilek <elior.papiernik@gmail.com>
2023-01-02 15:24:01 +0100

    Add get_reservation that only sends buttons

--- main.py
+++ main.py
@@ -20,12 +20,25 @@ async def help_command(update: telegram.Update, context: CallbackContext) -> any
     return
 
 
+async def get_reservations(update: telegram.Update, context: CallbackContext) -> any:
+    """Send a message to the user with buttons to click"""
+    keyboard = []
+    buttons = [str(i) for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
+    for button in buttons:
+        keyboard.append([telegram.InlineKeyboardButton(button, callback_data=button)])
+    reply_markup = telegram.InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text('Please choose:', reply_markup=reply_markup)
+    return
+
+
 def main():
     # Create application
     application = Application.builder().token(TOKEN).build()
 
     # Add handlers
     application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('help', help_command))
+    application.add_handler(CommandHandler('reservations', get_reservations))
     application.run_polling()
     return
 
