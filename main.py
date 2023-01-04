e60d4c572812236089afa2c2517dd7cbd2913dfd
Roilek <elior.papiernik@gmail.com>
2023-01-04 14:15:32 +0100

    Log path and token

--- main.py
+++ main.py
@@ -104,10 +104,9 @@ def main():
     if os.environ.get('ENV') == 'TEST':
         application.run_polling()
     elif os.environ.get('ENV') == 'PROD':
-        application.bot.setWebhook(HEROKU_PATH + TOKEN)
         application.run_webhook(listen="0.0.0.0",
                                 port=int(PORT),
-                                url_path=HEROKU_PATH+TOKEN)
+                                webhook_url=HEROKU_PATH+TOKEN)
     return
 
 
