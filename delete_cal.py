77fcc1a4ec7ad4bb5d415efa885532518bee8cd3
Roilek <elior.papiernik@gmail.com>
2023-01-04 11:17:09 +0100

    Add delete_cal to use graphical interface automation to remove events from the calendar

---
+++ delete_cal.py
@@ -0,0 +1,25 @@
+import time
+
+import pyautogui
+
+item_pos = (736, 233)
+
+
+def remove_events(n: int = 1) -> None:
+    pyautogui.hotkey('ctrl', 'left', interval=0.1)
+    time.sleep(0.25)
+    for i in range(n):
+        pyautogui.click(item_pos)
+        pyautogui.hotkey('delete', interval=0.1)
+    pyautogui.hotkey('ctrl', 'right', interval=0.1)
+    return
+
+
+def get_position() -> None:
+    print(pyautogui.position())
+    return
+
+
+if __name__ == '__main__':
+    get_position()
+    remove_events(0)
